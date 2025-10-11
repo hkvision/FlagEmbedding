581fd8d043b44b85530904a0523f03ccf92e2569
Kai Huang <huangkaivision@gmail.com>
2025-10-11 09:50:47 +0800

    Support unsloth (#3)
    * support unsloth
    
    * refactor config
    
    * remove device map and fp32

--- FlagEmbedding/finetune/reranker/encoder_only/base/runner.py
+++ FlagEmbedding/finetune/reranker/encoder_only/base/runner.py
@@ -22,13 +22,6 @@ class EncoderOnlyRerankerRunner(AbsRerankerRunner):
         Returns:
             Tuple[PreTrainedTokenizer, AbsEmbedderModel]: Tokenizer and model instances.
         """
-        tokenizer = AutoTokenizer.from_pretrained(
-            self.model_args.model_name_or_path,
-            cache_dir=self.model_args.cache_dir,
-            token=self.model_args.token,
-            trust_remote_code=self.model_args.trust_remote_code
-        )
-
         num_labels = 1
         config = AutoConfig.from_pretrained(
             self.model_args.config_name if self.model_args.config_name else self.model_args.model_name_or_path,
@@ -39,14 +32,44 @@ class EncoderOnlyRerankerRunner(AbsRerankerRunner):
         )
         logger.info('Config: %s', config)
 
-        base_model = AutoModelForSequenceClassification.from_pretrained(
-            self.model_args.model_name_or_path,
-            config=config,
-            cache_dir=self.model_args.cache_dir,
-            token=self.model_args.token,
-            from_tf=bool(".ckpt" in self.model_args.model_name_or_path),
-            trust_remote_code=self.model_args.trust_remote_code
-        )
+        use_unsloth = True
+        if use_unsloth:
+            from unsloth import FastLanguageModel, FastModel
+            import torch
+            base_model, tokenizer = FastModel.from_pretrained(
+                self.model_args.model_name_or_path,
+                auto_model=AutoModelForSequenceClassification,
+                max_seq_length=self.data_args.query_max_len + self.data_args.passage_max_len,  # 1024
+                config=config,
+                # dtype=torch.float32,  # align with original transformers
+                # dtype=torch.bfloat16,  # default to use bfloat16
+                full_finetuning=True,
+                load_in_4bit=False,
+                # device_map="xpu:0",  # will automatically use xpu and cuda if available
+                use_gradient_checkpointing=self.training_args.gradient_checkpointing,
+                # num_labels=1   # specified in config
+                cache_dir=self.model_args.cache_dir,
+                token=self.model_args.token,
+                from_tf=bool(".ckpt" in self.model_args.model_name_or_path),
+                trust_remote_code=self.model_args.trust_remote_code
+            )
+            print("==================Using Unsloth model==================")
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(
+                self.model_args.model_name_or_path,
+                cache_dir=self.model_args.cache_dir,
+                token=self.model_args.token,
+                trust_remote_code=self.model_args.trust_remote_code
+            )
+
+            base_model = AutoModelForSequenceClassification.from_pretrained(
+                self.model_args.model_name_or_path,
+                config=config,
+                cache_dir=self.model_args.cache_dir,
+                token=self.model_args.token,
+                from_tf=bool(".ckpt" in self.model_args.model_name_or_path),
+                trust_remote_code=self.model_args.trust_remote_code
+            )
 
         model = CrossEncoderModel(
             base_model,
